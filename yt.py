f7cd6e28575e8b81d2f4af742a618e034a08faba
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-05-27 20:19:49 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,11 +1,11 @@
 https://github.com/xtekky/google-login-bypass/blob/main/login.py  #replace email and pass in 7 & 8 line and replace 13 line with these
 'https://accounts.google.com/AddSession?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3D%252F&hl=en-GB&passive=false&service=youtube&uilel=0'
-#remove 19 to 22 lines 
-#After Merge With Below Code 
+#remove 19 to 22 lines in 1 line code
+#After Merge with 1st line code With Below Code 
 
 time.sleep(3)
 
-url = 'https://youtu.be/ZSeRbxXx8tY'
+url = 'https://youtu.be/ZSeRbxXx8tY' #replace with your required url
 driver.get(url)
 time.sleep(5)
 driver.find_element_by_css_selector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button').click()
